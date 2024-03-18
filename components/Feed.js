0b2018ee8770200ce115fe4558dfd3ff2ab0e107
Vinit Patidar <Vinitpatidar780@gmail.com>
2024-03-18 20:03:57 +0530

    .

--- components/Feed.js
+++ components/Feed.js
@@ -4,24 +4,25 @@ import Promptcard from './PromptCard';
 
 const Feed = () => {
     const [searchText, setSearchText] = useState('');
-    const [posts,setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
     const [filteredPosts, setFilteredPosts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchPosts();
-    },[]);
+    }, []);
 
     const fetchPosts = async () => {
         const response = await (await fetch('/api/prompt')).json();
         setPosts(response);
+        console.log(response);
         setFilteredPosts(response);
     }
 
     const filterPosts = (value) => {
-        if(value === ""){
+        if (value === "") {
             setFilteredPosts(posts);
-        }else{
-            setFilteredPosts(posts.filter((post)=>{
+        } else {
+            setFilteredPosts(posts.filter((post) => {
                 return post.tag.toLowerCase().includes(value.toLowerCase()) || post.prompt.toLowerCase().includes(value.toLowerCase()) || post.creator.username.includes(value.toLowerCase());
             }))
         }
@@ -36,23 +37,23 @@ const Feed = () => {
         filterPosts(tagValue);
         setSearchText(tagValue);
     }
-    
-  return (
-    <section className='feed'>
-        <form className="relative w-full flex-center">
-            <input type="text" placeholder='Search for a tag or a username' value={searchText} onChange={handleSearch} required className='search_input peer' />
-        </form>
-        <div className="mt-16 prompt_layout">
-            {
-                filteredPosts && 
-                    filteredPosts.map((post,index)=>{
-                        return <Promptcard post={post} key={post._id} handleTagClick={handleTagClick}/>
+
+    return (
+        <section className='feed'>
+            <form className="relative w-full flex-center">
+                <input type="text" placeholder='Search for a tag or a username' value={searchText} onChange={handleSearch} required className='search_input peer' />
+            </form>
+            <div className="mt-16 prompt_layout">
+                {
+                    filteredPosts &&
+                    filteredPosts.map((post, index) => {
+                        return <Promptcard post={post} key={post._id} handleTagClick={handleTagClick} />
                     })
-            }
-        </div>
-        {filteredPosts.length === 0 && <h1 className='text-center'>No records found</h1>}
-    </section>
-  )
+                }
+            </div>
+            {filteredPosts.length === 0 && <h1 className='text-center'>No records found</h1>}
+        </section>
+    )
 }
 
 export default Feed
